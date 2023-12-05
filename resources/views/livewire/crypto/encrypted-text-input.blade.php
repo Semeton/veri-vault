0e2e9063835b560a19b1a0ef304b5d2fd78d9037
Semeton <balogunsemeton@gmail.com>
2023-12-05 19:40:01 +0100

    Styling input forms

--- resources/views/livewire/crypto/encrypted-text-input.blade.php
+++ resources/views/livewire/crypto/encrypted-text-input.blade.php
@@ -2,7 +2,7 @@
     <div class="form-group">
         <div class="mt-2">
             <textarea id="plainText" name="plainText" rows="20" cols="100"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                class="block w-full rounded-md border-0 p-3 dark:text-gray-300 text-gray-700 shadow-sm ring-1 ring-inset dark:ring-gray-700 ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm block mt-1 w-full"
                 wire:model="encryptedText"></textarea>
         </div>
     </div>
