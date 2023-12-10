d0f3581491da3f552de5c7cb700a847cdd50055a
Semeton <balogunsemeton@gmail.com>
2023-12-10 20:36:09 +0100

    fix: userapi keys relationship changed to hasOne

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -60,9 +61,9 @@ class User extends Authenticatable
         'profile_photo_url',
     ];
 
-    public function apiKey(): HasMany
+    public function apiKey(): HasOne
     {
-        return $this->hasMany(ApiKey::class);
+        return $this->hasOne(ApiKey::class);
     }
 
     public function document(): HasMany
