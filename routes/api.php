98962b4b80e5e6e50fd3261fd7e77db69ee887a5
Semeton <balogunsemeton@gmail.com>
2023-12-17 13:06:54 +0100

    Implemented APIAuth for users with santum token

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthenticateApiKey;
+use App\Http\Controllers\Api\UserAuthController;
 use App\Http\Controllers\Api\MessageDecryptorController;
 use App\Http\Controllers\Api\MessageEncryptorController;
-use App\Http\Middleware\AuthenticateApiKey;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,14 +23,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
-    Route::middleware(['auth:sanctum'])->group(function(){
-        Route::post('/messages/encrypt', [MessageEncryptorController::class, 'encryptMessage']);
-        Route::post('/messages/decrypt', [MessageDecryptorController::class, 'decryptMessage']);
-    });
-});
-
-Route::get('/', function(){
+    Route::get('/', function(){
     return response()->json([
         'message' => 'Api server running'
     ]);
+});
+    Route::prefix('users')->group(function () {
+        Route::post('/register', [UserAuthController::class, 'register']);
+        Route::post('/login', [UserAuthController::class, 'login']);
+    });
+    Route::middleware(['auth:sanctum'])->group(function(){
+        Route::prefix('messages')->group(function () {
+            Route::post('/encrypt', [MessageEncryptorController::class, 'encryptMessage']);
+            Route::post('/decrypt', [MessageDecryptorController::class, 'decryptMessage']);
+        });
+    });
 });
