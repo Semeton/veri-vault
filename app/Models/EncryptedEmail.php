350f9a550d8706c08a27c85d7ceb82c221ae5007
Semeton <balogunsemeton@gmail.com>
2023-12-24 02:56:53 +0100

    patch: Create a uuid value for every encryptedemail model

--- app/Models/EncryptedEmail.php
+++ app/Models/EncryptedEmail.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class EncryptedEmail extends Model
 {
@@ -20,5 +21,14 @@ class EncryptedEmail extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = Str::uuid();
+        });
     }
 }
