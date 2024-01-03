f5633054eedf0670b6d262621a919630b0bb3d1a
Semeton <balogunsemeton@gmail.com>
2024-01-03 23:03:52 +0100

    installed semeton/crypto-service

--- app/Http/Controllers/Api/MessageDecryptorController.php
+++ app/Http/Controllers/Api/MessageDecryptorController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use App\Services\CryptoService;
+// use App\Services\CryptoService;
 use App\Http\Controllers\Controller;
+use Semeton\CryptoService\CryptoService;
 
 class MessageDecryptorController extends Controller
 {
